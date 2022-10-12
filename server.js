7caf0e82c5008900d96f6c321daa170f49f4b124
M-Nauman-Munir <naumanmunir.akbl@gmail.com>
2022-10-12 11:52:48 +0500

    simple nodejs app

--- server.js
+++ server.js
@@ -7,10 +7,19 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// routes
-app.use("/", require("./routes/goalRoutes"));
-app.use("/status", require("./routes/userRoutes"));
-app.use("/hello", require("./routes/userRoutes"));
+app.get("/", (req, res) => {
+  res.send("CI/CD App, Works well!");
+});
+
+app.get("/status", (req, res) => {
+  res.status(200);
+  res.send("Okay");
+});
+
+app.get("/hello", (req, res) => {
+  res.status(200);
+  res.send("Hi, there!");
+});
 
 app.listen(PORT, () =>
   console.log(`Server is running on Port: ${PORT}`.cyan.underline),
